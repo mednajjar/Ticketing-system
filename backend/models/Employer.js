dfced1c00839f12051a26ec1eb03b5c2da9b18f2
Mohammed Najjar <najjarmedd@gmail.com>
2021-05-06 18:44:15 +0800

    add employer schema

---
+++ backend/models/Employer.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const employerScheme = Schema({
+    nom: {type: String, min:3, max: 100, require: true},
+    prenom: {type: String, min:3, max: 100, require: true},
+    email: {type: String, require: true},
+    type: {type: String, ennum: ['employer', 'admin', 'technicien'], default: 'employer'},
+    password: {type: String, require: true},
+    id_departement: {type: Schema.Types.ObjectId, ref: 'Departement'},
+})
+
+module.exports = model('Employer', employerScheme)
