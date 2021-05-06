d16b1ff338534d1c20eac98a8301cd42dc093646
Mohammed Najjar <najjarmedd@gmail.com>
2021-05-06 18:59:54 +0800

    add ticket schema

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param {connection database} package 
+ */
+
 exports.db = (package) =>{
 package.connect(process.env.DB_CONNECTION, 
     {useUnifiedTopology: true,
@@ -6,3 +11,15 @@ package.connect(process.env.DB_CONNECTION,
 .then(()=>console.log('connection success!'))
 .catch(()=>console.log('connection failed!'))
 }
+
+/**
+ * @params (date format)
+ */
+
+exports.date= ()=>{
+    const d = new Date();
+    let month = d.getMonth() + 1;
+    if (month < 10) month = `0${month}`;
+    const dt = `${d.getFullYear()}-${month}-${d.getDate()}`;  
+}
+
---
+++ backend/models/Ticket.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+const {date} = require('../config/config')
+
+
+const ticketScheme = Schema({
+    titre: {type: String, min:3, max: 100, require: true},
+    type: {type: String, min:3, max: 100, require: true},
+    urgence: {type: String, ennum: ['normal', 'moyenne', 'urgent'], default: 'ticket'},
+    description: {type: String, min:10, max: 1024, require: true},
+    etat: {type: String, ennum:['waiting', 'assigned', 'reassiggned', 'resolved']},
+    date:{type: String, default: date},
+    id_employer: {type: Schema.Types.ObjectId, ref: 'Employer'},
+})
+
+module.exports = model('Ticket', ticketScheme)
